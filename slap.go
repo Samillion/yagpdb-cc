3c22683a113e28c1351caeaeb58ac8eb79263257
Sam <17427046+Samillion@users.noreply.github.com>
2023-10-05 12:40:00 +0300

    Update slap.go
    Improve error messages and be lenient with usage a bit. As long as the first argument is a valid user in the server, it should output a result.

--- slap.go
+++ slap.go
@@ -7,7 +7,7 @@
 */}}
 
 {{/* Adjust cooldown and GIFs (direct links) */}}
-{{ $cooldown := 180 }}
+{{ $cooldown := 30 }}
 {{ $images := cslice
 	"https://media.tenor.com/tKF3HiguDmEAAAAC/wrrruutchxxxxiii-slapt.gif"
 	"https://media.tenor.com/__oycZBexeAAAAAC/slap.gif"
@@ -20,42 +20,55 @@
 }}
 {{ $selfSlap := "https://media.tenor.com/rwipJN-E7okAAAAC/slap-slapping-self.gif" }}
 {{ $botSlap := "https://media.tenor.com/J9flx980Ot4AAAAC/reggie-deflecting-criticism.gif" }}
+{{ $usage := (print "```Usage: " .ServerPrefix "slap @user```") }}
 
 {{/* Do not edit beyond this point unless you know what you're doing. */}}
-{{ $usage := (print "```Usage: " .ServerPrefix "slap @user```") }}
-{{ $args := parseArgs 1 $usage (carg "userid" "User ID or mention") }}
-{{ $user := .User.ID }}
-{{ $target := ($args.Get 0) }}
-{{ if getMember $target }}
-	{{ if not (dbGet $user "slap") }}
-		{{ if gt $cooldown 0 }}
-			{{ dbSetExpire $user "slap" "cooldown" $cooldown }}
+{{ if (eq (len .Args) 2 3) }}
+	{{ $user := .User.ID }}
+	{{ $target := userArg (index .CmdArgs 0) }}
+
+	{{ if $target }}
+		{{ $target = $target.ID }}
+		{{ if not (dbGet $user "slap") }}
+			{{ if gt $cooldown 0 }}
+				{{ dbSetExpire $user "slap" "cooldown" $cooldown }}
+			{{ end }}
+			{{ $image := index $images (randInt (len $images)) }}
+			{{ $desc := (print "<@" $user "> slapped <@" $target ">.") }}
+			{{ if eq $target $user }}
+				{{ $desc = (print "<@" $user ">, why are you slapping yourself?") }}
+				{{ $image = $selfSlap }}
+			{{ else if eq $target 204255221017214977 }}
+				{{ $desc = (print "<@" $user ">, you think you can slap me? :rofl:") }}
+				{{ $image = $botSlap }}
+			{{ end }}
+			{{ $col := 16777215 }}
+			{{ $p := 0 }}
+			{{ $r := .Member.Roles }}
+			{{ range .Guild.Roles }}
+				{{- if and (in $r .ID) (.Color) (lt $p .Position) }}
+					{{- $p = .Position }}
+					{{- $col = .Color }}
+				{{- end -}}
+			{{- end }}
+			{{ $embed := cembed 
+				"description" $desc
+				"image" (sdict "url" $image)
+				"color" $col
+			}}
+			{{ sendMessage nil $embed }}
 		{{ end }}
-		{{ $image := index $images (randInt (len $images)) }}
-		{{ $desc := (print "<@" $user "> slapped <@" $target ">.") }}
-		{{ if eq $target $user }}
-			{{ $desc = (print "<@" $user ">, why are you slapping yourself?") }}
-			{{ $image = $selfSlap }}
-		{{ else if eq $target 204255221017214977 }}
-			{{ $desc = (print "<@" $user ">, you think you can slap me? :rofl:") }}
-			{{ $image = $botSlap }}
-		{{ end }}
-		{{ $col := 16777215 }}
-		{{ $p := 0 }}
-		{{ $r := .Member.Roles }}
-		{{ range .Guild.Roles }}
-			{{- if and (in $r .ID) (.Color) (lt $p .Position) }}
-				{{- $p = .Position }}
-				{{- $col = .Color }}
-			{{- end -}}
-		{{- end }}
+	{{ else }}
 		{{ $embed := cembed 
-			"description" $desc
-			"image" (sdict "url" $image)
-			"color" $col
+			"title" "Who dis?"
+			"description" (print "Defined user is not a member in this server." "\n\n" $usage)
 		}}
 		{{ sendMessage nil $embed }}
 	{{ end }}
 {{ else }}
-	{{ sendMessage nil (print "Defined user is not a member in this server." "\n" $usage) }}
+	{{ $embed := cembed 
+		"title" "Slap Command"
+		"description" (print "Do you want to slap someone? Yourself? Then this is the perfect command for you." "\n\n" $usage)
+	}}
+	{{ sendMessage nil $embed }}
 {{ end }}
