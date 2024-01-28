aa11f58ba32e054d20b2c8fbcedc6c4162d37229
Sam <17427046+Samillion@users.noreply.github.com>
2024-01-28 04:58:30 +0200

    Update confess.go

--- Confession/confess.go
+++ Confession/confess.go
@@ -1,14 +1,18 @@
-{{ $ch := 1198221854121402468 }}
+{{ $ch := 1200964297518497903 }}
+{{ $cd := 600 }}
+
 {{ $cmd := .Cmd }}
 {{ $msg := trimSpace .StrippedMsg }}
 {{ $usage := print "**Usage:**" "\n" "```" $cmd " your confession```" }}
 
 {{ $explain := print 
-	"This commnd will relay your message **__anonymously__** into the <#" $ch "> channel." "\n\n"
+	"This commnd will post your message **__anonymously__** to <#" $ch ">." "\n\n"
 	"**Explanation:**" "\n"
 	"- Once you use `-confess` it will post your message in the relative channel." "\n"
 	"- Your message will be deleted instantly, leaving only the anonymous one for others to read." "\n"
 	"- If the confession is less than 50 characters, nothing will be posted." "\n"
+	"- A limit of one confession every 10 minutes, globally." "\n"
+	"- Your initial post, even if it fails (ie: on cooldown), will instantly be deleted." "\n"
 	"- For your comfort, feel free to review the code of this command [here](https://github.com/Samillion/yagpdb-cc/tree/main/Confession)."
 }}
 
@@ -22,16 +26,28 @@
 	{{ return }}
 {{ end }}
 
+{{ $db := "confessions" }}
+{{ $ccid := .CCID }}
+
 {{ if $msg }}
 	{{ if gt (len $msg) 50 }}
-		{{ $note := print "Use " $cmd " to post your own, anonymously." }}
-		{{ $embed := cembed 
-			"title" "Anonymous Confession"
-			"description" $msg 
-			"color" 8421888
-			"footer" (sdict "text" $note)
-		}}
-		{{ sendMessage $ch $embed }}
+		{{ if not (dbGet $ccid $db) }}
+			{{ $note := print "Use " $cmd " to post your own, anonymously." }}
+			{{ $embed := cembed 
+				"title" "Anonymous Confession"
+				"description" $msg 
+				"color" 8421888
+				"footer" (sdict "text" $note)
+			}}
+			{{ sendMessage $ch $embed }}
+			{{ dbSetExpire $ccid $db "cooldown" $cd }}
+		{{ else }}
+			{{ $x := sendMessageRetID nil "There is a global 10 minutes cooldown per confession." }}
+			{{ deleteMessage nil $x 10 }}
+		{{ end }}
+	{{ else }}
+		{{ $x := sendMessageRetID nil "Your confession must contain at least 50 characters." }}
+		{{ deleteMessage nil $x 10 }}
 	{{ end }}
 	{{ deleteTrigger 0 }}
 {{ else }}
