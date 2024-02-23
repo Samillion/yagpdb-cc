493d27cdf1343e443c6b29d9253e882a04ae7cda
Sam <17427046+Samillion@users.noreply.github.com>
2024-02-23 05:49:44 +0200

    Update searchlink.go

--- Search Link/searchlink.go
+++ Search Link/searchlink.go
@@ -1,7 +1,7 @@
 {{ $engines := cslice
 	(sdict "trigger" "google" "name" "Google" "url" "https://www.google.com/search?hl=en&q=")
 	(sdict "trigger" "duck" "name" "DuckDuckGo" "url" "https://duckduckgo.com/?q=")
-	(sdict "trigger" "steam" "name" "Steam" "url" "https://store.steampowered.com/search/?term=")
+	(sdict "trigger" "brave" "name" "Brave Search" "url" "https://search.brave.com/search?q=")
 }}
 
 {{ $c := reReplace .ServerPrefix .Cmd "" | lower }}
@@ -9,25 +9,34 @@
 {{ $url := "" }}
 
 {{ range $engines }}
-	{{- if eq .trigger $c -}}
+	{{- if eq (lower .trigger) $c -}}
 		{{- $name = .name -}}
 		{{- $url = .url -}}
+		{{ break }}
 	{{- end -}}
 {{- end }}
 
 {{ $msg := trimSpace .StrippedMsg }}
 
 {{ if $msg }}
+	{{ if not $url }}
+		{{ sendMessage nil "**Error:** Search engine __$url__ is not set." }}
+		{{ return }}
+	{{ end }}
 	{{ $keys := (urlquery ($msg)) }}
 	{{ $link := print $url $keys }}
 	{{ sendMessage nil $link }}
 {{ else }}
+	{{ if not $name }}
+		{{ sendMessage nil "**Error:** Search engine __$name__ is not set." }}
+		{{ return }}
+	{{ end }}
 	{{ $desc := print "A simple link generator to search using **" $name "** because you're too lazy to open the browser and do it there." }}
 	{{ $usage := print "**Usage:**" "```" (lower .Cmd) " <keywords>```" }}
 	{{ $embed := cembed 
 		"title" "Search"
 		"description" (print $desc "\n\n" $usage )
-		"footer" (sdict "text" "Supported: google, duck, steam")
+		"footer" (sdict "text" "Supported: google, duck, brave")
 	}}
 	{{ sendMessage nil $embed }}
 {{ end }}
