c5aaa2a27930bd09c5f8910587b8d64d4a798605
Sam <17427046+Samillion@users.noreply.github.com>
2024-03-22 20:51:31 +0200

    Update searchlink.go
    - Add Wiki search
    - Cleanup

--- Search Link/searchlink.go
+++ Search Link/searchlink.go
@@ -1,42 +1,31 @@
-{{ $engines := cslice
-	(sdict "trigger" "google" "name" "Google" "url" "https://www.google.com/search?hl=en&q=")
-	(sdict "trigger" "duck" "name" "DuckDuckGo" "url" "https://duckduckgo.com/?q=")
-	(sdict "trigger" "brave" "name" "Brave Search" "url" "https://search.brave.com/search?q=")
+{{ $engines := sdict 
+	"google" (sdict "name" "Google" "url" "https://www.google.com/search?hl=en&q=")
+	"duck" (sdict "name" "DuckDuckGo" "url" "https://duckduckgo.com/?q=")
+	"brave" (sdict "name" "Brave Search" "url" "https://search.brave.com/search?q=")
+	"wiki" (sdict "name" "Wikipedia" "url" "https://www.wikipedia.org/wiki/")
 }}
 
-{{ $c := reReplace .ServerPrefix .Cmd "" | lower }}
-{{ $name := "" }}
-{{ $url := "" }}
+{{ $in := trimSpace .StrippedMsg }}
+{{ $cmd := slice .Cmd 1 (len .Cmd) | lower }}
 
-{{ range $engines }}
-	{{- if eq (lower .trigger) $c -}}
-		{{- $name = .name -}}
-		{{- $url = .url -}}
-		{{ break }}
-	{{- end -}}
-{{- end }}
+{{ $set := or ($engines.Get $cmd) $engines.google }}
+{{ $name := $set.name }}
+{{ $url := $set.url }}
 
-{{ $msg := trimSpace .StrippedMsg }}
-
-{{ if $msg }}
-	{{ if not $url }}
-		{{ sendMessage nil "**Error:** Search engine __$url__ is not set." }}
-		{{ return }}
+{{ if $in }}
+	{{ $keys := urlquery $in }}
+	{{ if eq $cmd "wiki" }}
+		{{ $keys = urlquery (joinStr "_" .CmdArgs) }}
 	{{ end }}
-	{{ $keys := (urlquery ($msg)) }}
 	{{ $link := print $url $keys }}
 	{{ sendMessage nil $link }}
 {{ else }}
-	{{ if not $name }}
-		{{ sendMessage nil "**Error:** Search engine __$name__ is not set." }}
-		{{ return }}
-	{{ end }}
-	{{ $desc := print "A simple link generator to search using **" $name "** because you're too lazy to open the browser and do it there." }}
+	{{ $desc := print "A simple link generator to search using **" $name "**." }}
 	{{ $usage := print "**Usage:**" "```" (lower .Cmd) " <keywords>```" }}
 	{{ $embed := cembed 
 		"title" "Search"
 		"description" (print $desc "\n\n" $usage )
-		"footer" (sdict "text" "Supported: google, duck, brave")
+		"footer" (sdict "text" "Supported: google, duck, brave, wiki")
 	}}
 	{{ sendMessage nil $embed }}
 {{ end }}
