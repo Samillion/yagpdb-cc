a5435624206bdcba1096f687911e124a8bdaeed0
Sam <17427046+Samillion@users.noreply.github.com>
2024-04-08 13:34:44 +0200

    Update randomstring.go
    Re-write and more options.

--- Random String/randomstring.go
+++ Random String/randomstring.go
@@ -1,27 +1,62 @@
-{{ $list := split "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_-+={[}]<,>.?" "" }}
-{{ $text := "" }}
-{{ $amount := 15 }}
-{{ $note := "" }}
+{{ $lower := "abcdefghijklmnopqrstuvwxyz" }}
+{{ $capital := upper $lower }}
+{{ $alpha := print $lower $capital }}
+{{ $num := "0123456789" }}
+{{ $sym := "~!@#$%&*" }}
+{{ $max := 60 }}
 
-{{ if gt (len .Args) 1 }}
-	{{ $input := toInt (index .CmdArgs 0) }}
-	
-	{{ if and (gt $input 0 ) (le $input 60) }}
-		{{ $amount = $input }}
-	{{ else if or (lt $input 1 ) (gt $input 60) }}
-		{{ $note = (print 
-			"**Note:**" "\n" 
-			"- Character count min is 1 and max is 60." "\n" 
-			"- Your input is: **" $input "**" "\n" 
-			"- Defaulted back to 15 chars output." 
-		) }}
+{{ $chars := sdict 
+	"alpha" $alpha
+	"num" $num
+	"sym" $sym
+	"capital" $capital
+	"lower" $lower
+	"mix" (print $alpha $num $sym)
+}}
+
+{{ $amount := 15 }}
+{{ $list := $chars.mix }}
+{{ $args := .CmdArgs }}
+
+{{ if $args }}
+	{{ $amount = index $args 0 | toInt }}
+	{{ if gt (len $args) 1 }}
+		{{ $type := index $args 1 | lower }}
+		{{ if $chars.HasKey $type }}
+			{{ $list = $chars.Get $type }}
+		{{ end }}
+	{{ end }}
+	{{ if $chars.HasKey (index $args 0 | lower) }}
+		{{ $amount = 15 }}
+		{{ $list = $chars.Get (index $args 0 | lower) }}
 	{{ end }}
 {{ end }}
 
+{{ $list = split $list "" }}
+
+{{ if or (lt $amount 1) (gt $amount $max) }}
+	{{ $amount = 15 }}
+{{ end }}
+
+{{ $result := "" }}
+
 {{ range (seq 0 $amount) }}
-    {{- $text = print $text (index (shuffle $list) 0) -}}
+    {{- $result = print $result (index (shuffle $list) 0) -}}
 {{- end }}
 
-{{ $text = print "**Random String:** (" $amount " chars)" "\n" "```" $text "```" }}
-{{ $text = print $text "\n" $note }}
-{{ sendMessage nil $text }}
+{{ $result = print "```" $result "```" }}
+
+{{ $explain := print 
+	"**Type Options:**" "\n"
+	"- alpha, lower, capital, num, sym, mix" "\n\n"
+	"**Character Limits:**" "\n"
+	"- Minimum 1, maximum " $max "\n"
+}}
+
+{{ $embed := cembed 
+	"title" "Random String"
+	"description" (print $amount " characters" $result "\n" $explain)
+	"footer" (sdict "text" "Usage: -random <amount:int> <type:str>")
+}}
+
+{{ sendMessage nil $embed }}
